940dee2cccffe502625ec378d05ab432011fb71a
Guilherme Pereira <guilospanck@protonmail.com>
2026-02-03 16:22:12 +0000

    ci: add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+const site = process.env.SITE_URL || "https://example.com";
+const base = process.env.BASE_PATH || "/";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site,
+  base,
+});
